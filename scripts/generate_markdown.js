0dbbb690f2aa7285f12dbd700365e521101b1f67
jnazare <jules.nazare@gmail.com>
2016-02-04 16:33:12 -0500

    cleanup

--- scripts/generate_markdown.js
+++ scripts/generate_markdown.js
@@ -9,10 +9,10 @@ function writeMarkdownToFile(filename, markdown){
 	}); 
 }
 
-var certschema = require('./certificate-schema-v2.json')
+var certschema = require('../schema/certificate-schema-v2.json')
 var certmarkdown = parse(certschema)
-writeMarkdownToFile('./certificate-schema-v2.md', certmarkdown)
+writeMarkdownToFile('../docs/certificate-schema-v2.md', certmarkdown)
 
-var issuerschema = require('./issuer-schema-v2.json')
+var issuerschema = require('../schema/issuer-schema-v2.json')
 var issuermarkdown = parse(issuerschema)
-writeMarkdownToFile('./issuer-schema-v2.md', issuermarkdown)
+writeMarkdownToFile('../docs/issuer-schema-v2.md', issuermarkdown)
