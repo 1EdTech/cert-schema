133df601fb95c8305a80c1fa4b058cd9cf44471a
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-09-06 15:44:45 -0700

    test framework

---
+++ tests/test_cert_schema.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestCertSchema(unittest.TestCase):
+    def test_cert_schema(self):
+        self.fail('needs tests!')
+
+
+if __name__ == '__main__':
+    unittest.main()
