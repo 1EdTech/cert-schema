6851ebe47fdf4a38684e6d08e8c20030701e6a2b
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2017-03-28 23:06:19 -0700

    increment version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'README.md')) as fp:
 
 setup(
     name='cert-schema',
-    version='2.0a1',
+    version='2.0a2',
     description='tools for working with blockchain certificates',
     author='MIT Media Lab Blockchain Certificates',
     tests_require=['tox'],
