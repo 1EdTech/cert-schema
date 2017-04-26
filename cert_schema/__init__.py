9d5edc2fbea051848bbf45ff8e390c81fbddfd9b
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2017-04-25 17:15:42 -0700

    v2 schema updates

--- cert_schema/__init__.py
+++ cert_schema/__init__.py
@@ -1,2 +1,5 @@
+__version__ = '2.0b2'
+
 from cert_schema.schema_tools.schema_validator import BlockcertValidationError
 from cert_schema.schema_tools.schema_validator import validate_unsigned_v1_2, validate_v1_2, validate_v2
+from cert_schema.schema_tools.document_loader import jsonld_document_loader
