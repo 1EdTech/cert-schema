7a76121d6a784534ca90a2d047d7a16e28053ac0
Kim (Hamilton) Duffy <kimdhamilton@gmail.com>
2017-05-02 16:51:05 -0700

    Revert "updates"
    This reverts commit 24caea03010ca96c0a6d4eb3b9b7b7ced794aeb4.

--- cert_schema/__init__.py
+++ cert_schema/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '2.0b3'
+__version__ = '2.0b2'
 
 from cert_schema.schema_tools.schema_validator import BlockcertValidationError
 from cert_schema.schema_tools.schema_validator import validate_unsigned_v1_2, validate_v1_2, validate_v2
