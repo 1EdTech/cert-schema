ea1a16698cfa63258902ca8a6af64a7467352614
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2017-10-24 20:23:13 +0200

    rev version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'README.md')) as fp:
 
 setup(
     name='cert-schema',
-    version='2.1.3',
+    version='2.1.4',
     description='Blockchain certificates JSON-LD context and JSON schemas',
     author='info@blockcerts.org',
     url='https://github.com/blockchain-certificates/cert-schema',
