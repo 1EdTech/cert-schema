57452501cca63122fd49dd4118ea279e949a6d01
Lucas Parker <fuerve@yahoo.com>
2018-07-30 13:46:42 -0700

    Updating setup.py with new version info.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'README.md')) as fp:
 
 setup(
     name='cert-schema',
-    version='2.1.5',
+    version='2.1.6',
     description='Blockchain certificates JSON-LD context and JSON schemas',
     author='info@blockcerts.org',
     url='https://github.com/blockchain-certificates/cert-schema',
@@ -21,6 +21,6 @@ setup(
     author_email='info@blockcerts.org',
     long_description=long_description,
     packages=find_packages(),
-    package_data={"cert_schema": ["1.1/*.json", "1.2/*.json", "2.0-alpha/*.json", "2.0/*.json"]},
+    package_data={"cert_schema": ["1.1/*.json", "1.2/*.json", "2.0-alpha/*.json", "2.0/*.json", "2.1/*.json"]},
     install_requires=reqs
 )
