19b1ed4be9cf0f961085da7261816275baddcc8b
Julien Fraichot <julien.fraichot@hyland.com>
2022-06-16 14:22:37 -0500

    refactor(Test): rename tests to better reflect test case

--- tests/normalization/v3/test_normalization_custom_properties.py
+++ tests/normalization/v3/test_normalization_custom_properties.py
@@ -9,39 +9,39 @@ class TestHashingProperties(unittest.TestCase):
     def test_credentialSubject_claim_degree_property(self):
         with open(os.path.join(os.path.dirname(__file__), './fixtures/v3-custom-context.json')) as data_f:
             certificate = json.load(data_f)
-            initial_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            initial_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
             certificate['credentialSubject']['claim']['degree'] = 'Licence of Puppets'
-            tempered_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
-            self.assertFalse(initial_hash == tempered_hash)
+            tempered_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            self.assertFalse(initial_normalized == tempered_normalized)
 
     def test_credentialSubject_claim_yearsOfAttendance_property(self):
         with open(os.path.join(os.path.dirname(__file__), './fixtures/v3-custom-context.json')) as data_f:
             certificate = json.load(data_f)
-            initial_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            initial_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
             certificate['credentialSubject']['claim']['yearsOfAttendance'] = '2023'
-            tempered_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
-            self.assertFalse(initial_hash == tempered_hash)
+            tempered_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            self.assertFalse(initial_normalized == tempered_normalized)
 
     def test_credentialSubject_claim_collegeName_property(self):
         with open(os.path.join(os.path.dirname(__file__), './fixtures/v3-custom-context.json')) as data_f:
             certificate = json.load(data_f)
-            initial_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            initial_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
             certificate['credentialSubject']['claim']['collegeName'] = 'Blockcerts University'
-            tempered_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
-            self.assertFalse(initial_hash == tempered_hash)
+            tempered_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            self.assertFalse(initial_normalized == tempered_normalized)
 
     def test_credentialSubject_claim_certificateNumber_property(self):
         with open(os.path.join(os.path.dirname(__file__), './fixtures/v3-custom-context.json')) as data_f:
             certificate = json.load(data_f)
-            initial_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            initial_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
             certificate['credentialSubject']['claim']['certificateNumber'] = '1234567890'
-            tempered_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
-            self.assertFalse(initial_hash == tempered_hash)
+            tempered_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            self.assertFalse(initial_normalized == tempered_normalized)
 
     def test_credentialSubject_claim_value_property(self):
         with open(os.path.join(os.path.dirname(__file__), './fixtures/v3-custom-context.json')) as data_f:
             certificate = json.load(data_f)
-            initial_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            initial_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
             certificate['credentialSubject']['claim']['value'] = '{\"this\":\"is the full improved transcript as cheated\"}'
-            tempered_hash = normalize_jsonld(certificate, detect_unmapped_fields=True)
-            self.assertFalse(initial_hash == tempered_hash)
+            tempered_normalized = normalize_jsonld(certificate, detect_unmapped_fields=True)
+            self.assertFalse(initial_normalized == tempered_normalized)
